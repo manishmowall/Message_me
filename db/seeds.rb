acda8a27e0fca81d9e723d47f8e1a7f93396c74f
manishmowall <manishmowall@gmail.com>
2021-09-29 16:48:40 +0530

    Still In Production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Manish", password: "singh")
+User.create(username: "Jhon", password: "jhon")
+User.create(username: "Geralt", password: "witcher")
+User.create(username: "Jones", password: "jones")
+User.create(username: "Tom", password: "tom")
+
+
